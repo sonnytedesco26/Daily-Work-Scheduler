5d00c973b11155b63ed7a3d870f8c5ec88c8f196
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-02-02 11:11:36 -0500

    Fixed js to not screw up class changing based on time, fixed up css to vertically center text. Still need @media edits and readme

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,8 @@
 $("#currentDay").html(moment().format('dddd, MMMM Do YYYY'));
 
 
-/*function saveClick(){
-    var time = $(this).parent().attr("id");
-    var task = $(this).siblings(".description").val();
-
-    localStorage.setItem(time, task);
-}*/
-
 $(document).ready(function(){
-    $(".saveBtn").click( function(){
+    $(".saveBtn").click(function(){
         var time = $(this).parent().attr("id");
         var task = $(this).siblings(".description").val();
 
@@ -20,26 +13,34 @@ $(document).ready(function(){
         var currentTime = moment().hour();
 
         $(".time-block").each(function(){
+            //0 was taking the wrong attribute
             var blockTime = parseInt($(this).attr("id").split("hour")[0]);
-
+            //since time "goes back down" after 12 (1 < 12) we need military time
+            //Needs to remove classes for if they're another case (otherwise overwriting gets messy)
             if(blockTime < currentTime){
+                $(this).removeClass("present");
+                $(this).removeClass("future");
                 $(this).addClass("past");
             } else if(blockTime === currentTime){
+                $(this).removeClass("past");
+                $(this).removeClass("future");
                 $(this).addClass("present");
             } else{
+                $(this).removeClass("past");
+                $(this).removeClass("present");
                 $(this).addClass("future");
             }
         })
     }
-    $("#9AM .description").val(localStorage.getItem("9AM"));
-    $("#10AM .description").val(localStorage.getItem("10AM"));
-    $("#11AM .description").val(localStorage.getItem("11AM"));
-    $("#12PM .description").val(localStorage.getItem("12PM"));
-    $("#1PM .description").val(localStorage.getItem("1PM"));
-    $("#2PM .description").val(localStorage.getItem("2PM"));
-    $("#3PM .description").val(localStorage.getItem("3PM"));
-    $("#4PM .description").val(localStorage.getItem("4PM"));
-    $("#5PM .description").val(localStorage.getItem("5PM"));
+    $("#9hour .description").val(localStorage.getItem("9hour"));
+    $("#10hour .description").val(localStorage.getItem("10hour"));
+    $("#11hour .description").val(localStorage.getItem("11hour"));
+    $("#12hour .description").val(localStorage.getItem("12hour"));
+    $("#13hour .description").val(localStorage.getItem("13hour"));
+    $("#14hour .description").val(localStorage.getItem("14hour"));
+    $("#15hour .description").val(localStorage.getItem("15hour"));
+    $("#16hour .description").val(localStorage.getItem("16hour"));
+    $("#17hour .description").val(localStorage.getItem("17hour"));
 
     timer();
 })
